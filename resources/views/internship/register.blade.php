46a135f4e8f8248bf3d8d480d23f2c5083d48193
Administrator <alvinkirana6@gmail.com>
2023-08-22 15:25:17 +0700

    convert template

---
+++ resources/views/internship/register.blade.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Academics &mdash; Website by Colorlib</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="landingpage/fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="landingpage/css/bootstrap.min.css">
+  <link rel="stylesheet" href="landingpage/css/jquery-ui.css">
+  <link rel="stylesheet" href="landingpage/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="landingpage/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="landingpage/css/owl.theme.default.min.css">
+
+  <link rel="stylesheet" href="landingpage/css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="landingpage/css/bootstrap-datepicker.css">
+
+  <link rel="stylesheet" href="landingpage/fonts/flaticon/font/flaticon.css">
+
+  <link rel="stylesheet" href="landingpage/css/aos.css">
+  <link href="landingpage/css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
+
+  <link rel="stylesheet" href="landingpage/css/style.css">
+  <style>
+      .color-a{
+        background-color: #f0b664;
+        color: #f0f0f0;
+      }
+      .color-b{
+        background-color: #1f4492;
+        color: #f0f0f0;
+      }
+
+      .text-color{
+        color: #1f4492;
+      }
+  </style>
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  <div class="site-wrap">
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    <div class="py-2 bg-light">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-9 d-none d-lg-block">
+            <a href="#" class="small mr-3"><span class="icon-question-circle-o mr-2"></span> Have a questions?</a> 
+            <a href="#" class="small mr-3"><span class="icon-phone2 mr-2"></span> 10 20 123 456</a> 
+            <a href="#" class="small mr-3"><span class="icon-envelope-o mr-2"></span> info@mydomain.com</a> 
+          </div>
+          <div class="col-lg-3 text-right">
+            <a href="login.html" class="small mr-3"><span class="icon-unlock-alt"></span> Log In</a>
+            <a href="register.html" class="small btn btn-primary px-4 py-2 rounded-0"><span class="icon-users"></span> Register</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+      <div class="container">
+        <div class="d-flex align-items-center">
+          <div class="site-logo">
+            <a href="index.html" class="d-block">
+              <img src="landingpage/images/logo.jpg" alt="Image" class="img-fluid">
+            </a>
+          </div>
+          <div class="mr-auto">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li>
+                  <a href="index.html" class="nav-link text-left">Home</a>
+                </li>
+                <li>
+                  <a href="courses.html" class="nav-link text-left">Apply Project</a>
+                </li>
+              </ul>                                                                                                                                                                                                                                                                                          </ul>
+            </nav>
+          </div>
+          <div class="ml-auto">
+            <div class="social-wrap">
+              <a href="#"><span class="icon-facebook"></span></a>
+              <a href="#"><span class="icon-twitter"></span></a>
+              <a href="#"><span class="icon-linkedin"></span></a>
+
+              <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+                class="icon-menu h3"></span></a>
+            </div>
+          </div>
+         
+        </div>
+      </div>
+
+    </header>
+
+    
+
+    
+
+    <div class="custom-breadcrumns border-bottom">
+      <div class="container">
+        <a href="index.html">Home</a>
+        <span class="mx-3 icon-keyboard_arrow_right"></span>
+        <span class="current">Register</span>
+      </div>
+    </div>
+
+    <div class="site-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                  <div class="card-body color-b" style="border-radius: 10px;">
+                    <div class="row">
+                        <div class="col-md-12 form-group">
+                            <label for="username">Username</label>
+                            <input type="text" id="username"  class="form-control form-control-sm" placeholder="input username ...">
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" class="form-control form-control-sm" placeholder="input email ...">
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <label for="pword">Password</label>
+                            <input type="password" id="pword" class="form-control form-control-sm" placeholder="input password ...">
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <label for="pword2">Re-type Password</label>
+                            <input type="password" id="pword2" class="form-control form-control-sm" placeholder="retype password ...">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <a href="dashboard.html" class="btn color-a btn-sm px-5">Register</a>
+                            <button type="reset" class="btn btn-sm btn-secondary">Cancel</button>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            
+
+          
+        </div>
+    </div>
+
+    
+
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <p class="mb-4"><img src="landingpage/images/logo.png" alt="Image" class="img-fluid"></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo minima qui dolor, iusto iure.</p>  
+            <p><a href="#">Learn More</a></p>
+          </div>
+          <div class="col-lg-3">
+            <h3 class="footer-heading"><span>Our Campus</span></h3>
+            <ul class="list-unstyled">
+                <li><a href="#">Acedemic</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Our Interns</a></li>
+                <li><a href="#">Our Leadership</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Human Resources</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-3">
+              <h3 class="footer-heading"><span>Our Courses</span></h3>
+              <ul class="list-unstyled">
+                  <li><a href="#">Math</a></li>
+                  <li><a href="#">Science &amp; Engineering</a></li>
+                  <li><a href="#">Arts &amp; Humanities</a></li>
+                  <li><a href="#">Economics &amp; Finance</a></li>
+                  <li><a href="#">Business Administration</a></li>
+                  <li><a href="#">Computer Science</a></li>
+              </ul>
+          </div>
+          <div class="col-lg-3">
+              <h3 class="footer-heading"><span>Contact</span></h3>
+              <ul class="list-unstyled">
+                  <li><a href="#">Help Center</a></li>
+                  <li><a href="#">Support Community</a></li>
+                  <li><a href="#">Press</a></li>
+                  <li><a href="#">Share Your Story</a></li>
+                  <li><a href="#">Our Supporters</a></li>
+              </ul>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+  </div>
+  <!-- .site-wrap -->
+  <script src="landingpage/js/jquery-3.3.1.min.js"></script>
+  <script src="landingpage/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="landingpage/js/jquery-ui.js"></script>
+  <script src="landingpage/js/popper.min.js"></script>
+  <script src="landingpage/js/bootstrap.min.js"></script>
+  <script src="landingpage/js/owl.carousel.min.js"></script>
+  <script src="landingpage/js/jquery.stellar.min.js"></script>
+  <script src="landingpage/js/jquery.countdown.min.js"></script>
+  <script src="landingpage/js/bootstrap-datepicker.min.js"></script>
+  <script src="landingpage/js/jquery.easing.1.3.js"></script>
+  <script src="landingpage/js/aos.js"></script>
+  <script src="landingpage/js/jquery.fancybox.min.js"></script>
+  <script src="landingpage/js/jquery.sticky.js"></script>
+  <script src="landingpage/js/jquery.mb.YTPlayer.min.js"></script>
+
+
+
+
+  <script src="landingpage/js/main.js"></script>
+
+</body>
+
+</html>
