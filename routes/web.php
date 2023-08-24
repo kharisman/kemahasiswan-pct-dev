a121781676f766b5725b864782b753526bb1d4ea
Rizki.Muzaki <twaty2512@gmail.com>
2023-08-25 04:34:12 +0700

    admin-blade

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Tes admin blade
+Route::get('/admin-dashboard', function () { return view('admin/dashboard');});
+Route::get('/admin-artikel', function () { return view('admin/artikel');});
+Route::get('/admin-confirm-iduka', function () { return view('admin/confirm_iduka');});
+Route::get('/admin-confirm-internship', function () { return view('admin/confirm_internship');});
+Route::get('/admin-event', function () { return view('admin/event');});
+Route::get('/admin-iduka', function () { return view('admin/iduka');});
+Route::get('/admin-internship', function () { return view('admin/internship');});
