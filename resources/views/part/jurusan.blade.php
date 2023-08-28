63db62f42e94434f60f87d1a520866250d5f4ea8
kharisma elan <elsevenwriter@gmail.com>
2023-08-28 11:23:12 +0700

    update landing page

---
+++ resources/views/part/jurusan.blade.php
@@ -0,0 +1,21 @@
+<div class="section-bg style-1" style="background-image: route('images/hero_1.jpg');">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+                <span class="icon flaticon-mortarboard"></span>
+                <h3>Tekhnik Informatika </h3>
+                <p>Menjadi program studi yang menghasilkan sarjana dengan kompetensi unggulan di bidang rekayasa perangkat lunak, pengembangan sistem cerdas dan jaringan komputer yang berprestasi dan berwawasan global.</p>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+                <span class="icon flaticon-school-material"></span>
+                <h3>Sistem Informasi</h3>
+                <p>Menjadi program studi yang menghasilkan sarjana dengan kompetensi unggulan di bidang desain, manajemen dan analisis sistem informasi yang berprestasi dan berwawasan global.</p>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+                <span class="icon flaticon-library"></span>
+                <h3>Bisnis Digital</h3>
+                <p>Menjadi program studi yang menghasilkan sarjana dengan kompetensi di bidang bisnis dan keuangan dengan berbasis teknologi digital yang berprestasi dan berwawasan global.</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/part/slider.blade.php
@@ -0,0 +1,14 @@
+<div class="hero-slide owl-carousel site-blocks-cover">
+    <div class="intro-section" style="background-image: url('{{asset('landingpage/images/alamat_palcomtech.png')}}');">
+
+    </div>
+
+    <div class="intro-section" style="background-image: url('{{asset('landingpage/images/smart_campus.jpg')}}');">
+        <div class="container">
+            <div class="row align-items-center">
+
+            </div>
+        </div>
+    </div>
+
+</div>
