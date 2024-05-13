e3ba692ba06b784bfe309e99da5e7cc705808065
kharisma elan <elsevenwriter@gmail.com>
2024-05-13 11:27:06 +0700

    last update perbaikan natasi saja

---
+++ app/Models/pctKuisOpsi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pctKuisOpsi extends Model
+{
+    use HasFactory;
+
+    
+    protected $table = 'pct_kuis_opsi';
+
+    
+}
